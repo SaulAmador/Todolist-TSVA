d59872eac7c4e1f2bb8e8513bf6ae8bc3b166cca
Saul Amador <155937983+SaulAmador@users.noreply.github.com>
2025-04-15 02:17:10 +0000

    todolist-terminado-TSVA

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+    const [tareas, setTareas] = useState([]); 
+    const [nuevaTarea, setNuevaTarea] = useState(''); 
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    
+    const manejarEnter = (e) => {
+        if (e.key === 'Enter' && nuevaTarea.trim()) {
+            setTareas([...tareas, { id: Date.now(), texto: nuevaTarea }]); 
+            setNuevaTarea(''); 
+        }
+    };
+
+    
+    const eliminarTarea = (id) => {
+        setTareas(tareas.filter(tarea => tarea.id !== id)); 
+    };
+
+    return (
+        <div className="text-center">
+            <h1 className="text-center mt-5">Goals for Today</h1>
+            <input
+                type="text"
+                value={nuevaTarea}
+                onChange={(e) => setNuevaTarea(e.target.value)}
+                onKeyDown={manejarEnter}
+                placeholder="Write your goal..."
+            />
+            
+            <div className="lista-tareas">
+                {tareas.length === 0 ? (
+                    <p className="mensaje-vacio">Small steps, big impact.</p>
+                ) : (
+                    tareas.map((tarea) => (
+                        <Tarea key={tarea.id} tarea={tarea} eliminarTarea={eliminarTarea} />
+                    ))
+                )}
+            </div>
+        </div>
+    );
 };
+
+
+function Tarea({ tarea, eliminarTarea }) {
+    return (
+        <div className="tarea-contenedor">
+            <span>{tarea.texto}</span>
+            <button 
+                className="boton-eliminar"
+                onClick={() => eliminarTarea(tarea.id)}
+            >
+                ❌
+            </button>
+        </div>
+    );
+}
 
 export default Home;
